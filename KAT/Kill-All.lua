758baec479f9ec17f25dfbbfebc65be5acf7c02d
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-06-12 06:41:24 -0400

    Update Kill-All.lua

--- KAT/Kill-All.lua
+++ KAT/Kill-All.lua
@@ -1,11 +1,22 @@
-local key = game:GetService("UserInputService") --- this is meant for KAT but works for any game
-key.InputBegan:Connect(function(input)
-if input.KeyCode == Enum.KeyCode.E then
-for i,v in pairs(game.Players:GetChildren()) do
-    if v ~= game.Players.LocalPlayer then
-         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.Head.CFrame * CFrame.new(0,0,.4)
-         wait(1)
+-- Modify by YellowGreg#7993
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+
+UserInputService.InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.E then
+        for i, player in pairs(Players:GetPlayers()) do
+            if player ~= Players.LocalPlayer then
+                local localPlayerCharacter = Players.LocalPlayer.Character
+                local targetCharacter = player.Character
+
+                if localPlayerCharacter and targetCharacter and targetCharacter:FindFirstChild("Head") then
+                    local humanoidRootPart = localPlayerCharacter:WaitForChild("HumanoidRootPart")
+                    local targetHead = targetCharacter.Head
+
+                    humanoidRootPart.CFrame = targetHead.CFrame * CFrame.new(0, 0, 0.4)
+                    wait(1)
+                end
+            end
+        end
     end
-end
-end
 end)
