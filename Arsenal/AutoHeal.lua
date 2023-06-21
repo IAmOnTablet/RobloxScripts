0cbcb2b25f0da29ffafb64b4b986b65c37ad1848
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-06-21 08:27:44 -0400

    Update AutoHeal.lua

--- Arsenal/AutoHeal.lua
+++ Arsenal/AutoHeal.lua
@@ -1,14 +1,33 @@
 --// Made By YellowGreg \\--
--- Still in Development
-spawn(function()
-    wait()
-    pcall(function()
-        for i, v in pairs(Workspace.Debris:GetChildren()) do
-          if v.Name == "DeadHP" then
-            v.CFrame = Player.Character.HumanoidRootPart.CFrame
-          end
+local autoHealEnabled = true
+
+local function autoHeal()
+    spawn(function()
+        while autoHealEnabled do
+            wait()
+            pcall(function()
+                local player = game.Players.LocalPlayer
+                local character = player.Character
+                local humanoidRootPart = character.HumanoidRootPart
+
+                for i, v in pairs(game.Workspace.Debris:GetChildren()) do
+                    if v.Name == "DeadHP" then
+                        if autoHealEnabled then
+                            v.CFrame = humanoidRootPart.CFrame
+                        else
+                            print("Auto healing is turned off. DeadHP will not teleport.")
+                        end
+                    end
+                end
+            end)
         end
-      end
-    )
-  end
-end)
+    end)
+end
+
+-- Function to toggle auto heal on or off
+local function toggleAutoHeal(enabled)
+    autoHealEnabled = enabled
+    if enabled then
+        autoHeal()
+    end
+end
